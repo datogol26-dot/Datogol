ea4efb9c84929d9c0d8e125278fb0c0ebf072462
datogol26-dot <datogol26@gmail.com>
2026-03-01 11:27:51 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
+alert("DatoGol ya esta funcionando");
+
 document.addEventListener("DOMContentLoaded", function() {
 
   const match = {
     home: {
-      name: "Manchester City"
+      name: "Manchester City",
+      points: 57
     },
     away: {
-      name: "Arsenal"
+      name: "Arsenal",
+      points: 60
     }
   };
 
   document.getElementById("home-name").textContent = match.home.name;
   document.getElementById("away-name").textContent = match.away.name;
+
+  document.getElementById("home-points").textContent =
+    match.home.points + " pts";
+
+  document.getElementById("away-points").textContent =
+    match.away.points + " pts";
 
 });
